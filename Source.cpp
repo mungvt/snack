666f2dac8d14bc25a145a612aa6eea4a6d998b0c
Vu Tien Mung 20173261 <mung.vt173261@sis.hust.edu.vn>
2019-04-20 16:52:12 +0700

    Thêm map bot, debug một số lỗi, thêm tính năng điều khiển menu

--- Source.cpp
+++ Source.cpp
@@ -9,8 +9,9 @@
 #include "console.h"
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
-#define consoleWidth	80
+#define consoleWidth	85
 #define consoleHeight	30
 
 enum TrangThai { UP, DOWN, LEFT, RIGHT };
@@ -27,6 +28,7 @@ struct ToaDo
 struct HoaQua
 {
 	ToaDo td;
+	ToaDo tim;
 };
 
 
@@ -52,7 +54,8 @@ int ChonMap() {
 	printf("2. EEG Map\n");
 	printf("3. Never Die\n");
 	printf("4. Snake Bot\n");
-	printf("5. Exit");
+	printf("5. Exit\n");
+	printf("Your choose: ");
 	int i = 0;
 	scanf("%d", &i);
 	return i;
@@ -66,8 +69,10 @@ void KhoiTao(Snake& snake, HoaQua& hq)
 
 	snake.tt = RIGHT;
 
-	hq.td.x = 2;
+	hq.td.x = 20;
 	hq.td.y = 1;
+	hq.tim.x = 25;
+	hq.tim.y = 5;
 
 	pastPress[UP] = false;
 	pastPress[DOWN] = false;
@@ -165,10 +170,19 @@ void HienThi(Snake snake, HoaQua hq)
 		else 
 			putchar('#');// xóa cái đuôi.
 	}
+	// in điểm, mạng
+	gotoXY(consoleWidth + 5, 0);
+	printf("Your Score: %d", snake.n);
+	gotoXY(consoleWidth + 5, 1);
+	printf("Your Heart: %d", 3);
+	gotoXY(consoleWidth + 5, 4);
+	printf("Press Space to Return menu!");
+	gotoXY(consoleWidth + 5, 5);
+	printf("Press Enter to Quit!");
 }
 
 void HienThiBot(Snake snakeBot) {
-	TextColor(ColorCode_Pink);
+	TextColor(ColorCode_Yellow);
 	gotoXY(snakeBot.dot[0].x, snakeBot.dot[0].y);
 	putchar(2);
 	//in nốt phần thân còn lại.
@@ -191,6 +205,14 @@ void DieuKhien_DiChuyen(Snake& snake)
 	{
 		int key = _getch();
 
+		// Chọn lại map
+		if (key == ' ') {
+			clrscr();
+			chooseMap = ChonMap();
+			clrscr();
+		}
+		// Thoát Game
+		if (key == 13) exit(0);
 		// điều khiển cái đầu mà thôi
 		if ((key == 'A' || key == 'a') && pastPress[RIGHT] == false)
 		{
@@ -248,6 +270,10 @@ void DieuKhien_DiChuyen(Snake& snake)
 
 void DieuKhien_DiChuyen_Bot(Snake& snakeBot, HoaQua hq) {
 
+	if ((hq.td.x < snakeBot.dot[0].x) && (snakeBot.tt != RIGHT)) snakeBot.tt = LEFT;
+	if ((hq.td.x > snakeBot.dot[0].x) && (snakeBot.tt != LEFT)) snakeBot.tt = RIGHT;
+	if ((hq.td.y < snakeBot.dot[0].y) && (snakeBot.tt != DOWN)) snakeBot.tt = UP;
+	if ((hq.td.y > snakeBot.dot[0].y) && (snakeBot.tt != UP)) snakeBot.tt = DOWN;
 	for (int i = snakeBot.n; i > 0; i--)
 		snakeBot.dot[i] = snakeBot.dot[i - 1];
 
@@ -313,24 +339,53 @@ int XuLy(Snake & snake, Snake & snakeBot, HoaQua & hq, int& ThoiGianSleep, int c
 		snakeBot.tt = DOWN;
 	}
 	// Xử lý cắn vào thân	
-	for (int i = 1; i < snake.n; i++)
+	for (int i = 1; i < snake.n; i++) {
 		if (snake.dot[0].x == snake.dot[i].x && snake.dot[0].y == snake.dot[i].y) {
-			if(chooseMap != 3) return -1;// Chết.
+			if (chooseMap != 3) return -1;// Chết.
 			if (chooseMap == 3) {
+				for (int j = snake.n; j > i; j--) {
+					gotoXY(snake.dot[j].x, snake.dot[j].y);
+					putchar(' ');
+				}
 				snake.n = i;// Không chết.
 			}
+
 		}
+		// nếu rắn bot cắn rắn người thi thua game.
+		if (chooseMap == 4) {
+			if (snake.dot[i].x == snakeBot.dot[0].x && snake.dot[i].y == snakeBot.dot[0].y) {
+				return -1;
+			}
+		}
+
+	}
+	for (int i = 1; i < snakeBot.n; i++) {
+		if (snakeBot.dot[0].x == snakeBot.dot[i].x && snakeBot.dot[0].y == snakeBot.dot[i].y) {// nếu rắn bot tự cắn vào thân thì.
+			for (int i = snakeBot.n; i > 0; i--) {
+				gotoXY(snakeBot.dot[i].x, snakeBot.dot[i].y);
+				putchar(' ');// xóa hết con rắn bot == coi như nó bị chết rồi xuất hiện bot mới.
+				snakeBot.n = 1;
+				snakeBot.dot[0].x = rand() % (consoleWidth - 1) + 1;
+				snakeBot.dot[0].y = rand() % (consoleHeight - 1) + 1;
+			}
+		}
+		if (snake.dot[0].x == snakeBot.dot[i].x && snake.dot[0].y == snakeBot.dot[i].y) {// nếu rắn người cắn vào bot thì bot bị khỏi tạo lại
+			for (int j = snakeBot.n; j >= 0; j--) {
+				gotoXY(snakeBot.dot[j].x, snakeBot.dot[j].y);
+				putchar(' ');// xóa hết con rắn bot == coi như nó bị chết rồi xuất hiện bot mới.
+				snakeBot.n = 1;
+				snakeBot.dot[0].x = rand() % (consoleWidth - 1) + 1;
+				snakeBot.dot[0].y = rand() % (consoleHeight - 1) + 1;
+			}
+		}
+	}
 	//xu ly ran can vao map
 	if (chooseMap == 2) 
 		if (map[snake.dot[0].y][snake.dot[0].x] == 1)
 		return -1;
-			
+	// rắn người ăn được hoa quả	
 	if (snake.dot[0].x == hq.td.x && snake.dot[0].y == hq.td.y)
 	{
-		// ăn được hoa quả
-		// việc ăn được hoa quả rồi mới chèn thêm cái đầu mới thì thật sự chưa giải quyết tốt cho lắm
-		// các bạn cần suy nghĩ thêm để tốt hơn
-		
 		for (int i = snake.n + 1; i > 0; i--)
 			snake.dot[i] = snake.dot[i - 1];
 
@@ -346,18 +401,42 @@ int XuLy(Snake & snake, Snake & snakeBot, HoaQua & hq, int& ThoiGianSleep, int c
 			snake.dot[0].x++;
 
 
-		hq.td.x = rand() % consoleWidth; 
-		hq.td.y = rand() % consoleHeight;
-		while (map[hq.td.y][hq.td.x]==1)
-		{
-			hq.td.y = rand() % consoleHeight;
+		hq.td.x = rand() % (consoleWidth - 1) + 1;
+
+		hq.td.y = rand() % (consoleHeight - 1) + 1;
+
 			
+		if (chooseMap == 2) {
+			while (map[hq.td.y][hq.td.x] == 1)
+			{
+				hq.td.y = rand() % consoleHeight;
+			}
 		}
-	
-		if (snake.n % 3 == 0){
-			if (ThoiGianSleep > 30)
-				ThoiGianSleep -= 20;
-		}
+		if (ThoiGianSleep > 30)
+			ThoiGianSleep -= 10;
+	}
+	// rắn bot ăn được hoa quả
+	if (snakeBot.dot[0].x == hq.td.x && snakeBot.dot[0].y == hq.td.y)
+	{
+		for (int i = snakeBot.n + 1; i > 0; i--)
+			snakeBot.dot[i] = snakeBot.dot[i - 1];
+
+		snakeBot.n++;
+
+		if (snakeBot.tt == UP)
+			snakeBot.dot[0].y--;
+		else if (snakeBot.tt == DOWN)
+			snakeBot.dot[0].y++;
+		else if (snakeBot.tt == LEFT)
+			snakeBot.dot[0].x--;
+		else if (snakeBot.tt == RIGHT)
+			snakeBot.dot[0].x++;
+
+		hq.td.x = rand() % (consoleWidth - 1) + 1;
+
+		hq.td.y = rand() % (consoleHeight - 1) + 1;
+		//if (ThoiGianSleep > 30)
+			//ThoiGianSleep -= 3 * snakeBot.n;
 	}
 	return 0;
 }
@@ -369,7 +448,7 @@ int main()
 	Snake snake, snakeBot;
 	HoaQua hq;
 	int ma = 0;
-	int ThoiGianSleep = 100;
+	int ThoiGianSleep = 150;
 
 	chooseMap = ChonMap();
 	clrscr();
@@ -377,6 +456,7 @@ int main()
 	KhoiTao(snake, hq);
 	KhoiTaoBot(snakeBot);
 
+	if (chooseMap == 5) goto endgame;
 	while (1)
 	{
 		Nocursortype();
@@ -398,7 +478,7 @@ int main()
 		if (ma == -1)	// thua game đó nha
 		{
 			gotoXY(consoleWidth + 1, 10);
-			printf("Ban thua roi, liu liu");
+			printf("Ban thua roi, liu liu\n");
 
 			while (_getch() != 13);
 
@@ -407,6 +487,9 @@ int main()
 
 		if (pastPress[LEFT] || pastPress[RIGHT]) Sleep(ThoiGianSleep / 2);
 		else Sleep(ThoiGianSleep);
+		gotoXY(consoleWidth + 5, 10);
+		printf("%d",ThoiGianSleep);
+
 	}
-	return 0;
+	endgame: return 0;
 }
